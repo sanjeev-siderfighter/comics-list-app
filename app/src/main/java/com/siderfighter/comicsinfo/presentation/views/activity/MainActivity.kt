c924f0a1f7387c5fcbad4791fc2d2bb9a8cbd1bb
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-12 02:09:32 +0530

    chore: Observe the response of the api

--- app/src/main/java/com/siderfighter/comicsinfo/presentation/views/activity/MainActivity.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/presentation/views/activity/MainActivity.kt
@@ -2,17 +2,25 @@ package com.siderfighter.comicsinfo.presentation.views.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
 import com.siderfighter.comicsinfo.R
 import com.siderfighter.comicsinfo.presentation.viewmodel.RajComicsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    @Inject lateinit var viewModel: RajComicsViewModel
+    private val viewModel: RajComicsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewModel.getRajComicsPage1()
+
+        viewModel.rajComicsPage1.observe(this) {
+            findViewById<TextView>(R.id.tv_hello).text = it.data.toString()
+        }
     }
 }
