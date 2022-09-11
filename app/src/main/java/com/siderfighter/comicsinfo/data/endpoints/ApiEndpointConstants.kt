6dfcab8c2970b9d98f48bbb8823f3b518cf62c91
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-12 00:27:27 +0530

    feat: Add the endpoint for raj comics page 1

---
+++ app/src/main/java/com/siderfighter/comicsinfo/data/endpoints/ApiEndpointConstants.kt
@@ -0,0 +1,5 @@
+package com.siderfighter.comicsinfo.data.endpoints
+
+object ApiEndpointConstants {
+    const val RAJ_COMICS_PAGE_1 = "AKfycbx7owrZ-NZspGuyHSGnyOZWC10uVyI8hqLm1AaHEBo/dev"
+}
---
+++ app/src/main/java/com/siderfighter/comicsinfo/data/repository/NetworkInterface.kt
@@ -0,0 +1,10 @@
+package com.siderfighter.comicsinfo.data.repository
+
+import com.siderfighter.comicsinfo.data.endpoints.ApiEndpointConstants.RAJ_COMICS_PAGE_1
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface NetworkInterface {
+    @GET(RAJ_COMICS_PAGE_1)
+    suspend fun getRajComicsPage1(): Response<String> // todo: Change to appropriate object
+}
