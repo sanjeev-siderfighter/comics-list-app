2028fd02cf3ce4ec05c8b1309b613cd12d2edf14
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-12 00:05:27 +0530

    :tada: Basic Project Setup

---
+++ app/src/main/java/com/siderfighter/comicsinfo/application/ComicsListApp.kt
@@ -0,0 +1,8 @@
+package com.siderfighter.comicsinfo.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComicsListApp : Application() {
+}
