ec4b6780fa6980c07187701d82a633d4d9799d19
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-12 00:31:45 +0530

    fix: Return the correct retrofit instance for raj comics interface

--- app/src/main/java/com/siderfighter/comicsinfo/network/RetrofitInstance.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/network/RetrofitInstance.kt
@@ -1,20 +1,23 @@
 package com.siderfighter.comicsinfo.network
 
+import com.siderfighter.comicsinfo.data.repository.RajComicsRepository
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 private const val RAJ_COMICS_INFO_BASE_URL = "https://script.google.com/macros/s/" // todo: move to a secure location
 
 object RetrofitInstance {
-    fun getInstance(): Retrofit {
-        val retrofitBuilder = Retrofit.Builder()
+    fun getRajComicsInstance(): RajComicsRepository {
+        val retrofit = Retrofit.Builder()
             .baseUrl(RAJ_COMICS_INFO_BASE_URL)
             .client(getLoggingInterceptor().build())
             .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-        return retrofitBuilder.build()
+        return retrofit.create(RajComicsRepository::class.java)
     }
 
     private fun getLoggingInterceptor(): OkHttpClient.Builder {
