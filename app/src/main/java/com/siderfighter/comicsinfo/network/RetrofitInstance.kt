6b77dcd046cdc705da701c7e460007b7e2b90430
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-12 01:34:07 +0530

    fix: Return type should be RajComicsNetworkInterface

--- app/src/main/java/com/siderfighter/comicsinfo/network/RetrofitInstance.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/network/RetrofitInstance.kt
@@ -1,6 +1,6 @@
 package com.siderfighter.comicsinfo.network
 
-import com.siderfighter.comicsinfo.data.repository.rajcomics.RajComicsRepository
+import com.siderfighter.comicsinfo.data.endpoints.RajComicsNetworkInterface
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -9,14 +9,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 private const val RAJ_COMICS_INFO_BASE_URL = "https://script.google.com/macros/s/" // todo: move to a secure location
 
 object RetrofitInstance {
-    fun getRajComicsInstance(): RajComicsRepository {
+    fun getRajComicsInstance(): RajComicsNetworkInterface {
         val retrofit = Retrofit.Builder()
             .baseUrl(RAJ_COMICS_INFO_BASE_URL)
             .client(getLoggingInterceptor().build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-        return retrofit.create(RajComicsRepository::class.java)
+        return retrofit.create(RajComicsNetworkInterface::class.java)
     }
 
     private fun getLoggingInterceptor(): OkHttpClient.Builder {
