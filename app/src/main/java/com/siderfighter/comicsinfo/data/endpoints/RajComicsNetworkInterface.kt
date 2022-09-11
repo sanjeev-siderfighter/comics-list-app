c66612a33148e8a9865dcd41c697bb057d36a8b9
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-12 00:32:48 +0530

    rename to RajComicsNetworkInterface

--- app/src/main/java/com/siderfighter/comicsinfo/data/endpoints/RajComicsNetworkInterface.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/data/endpoints/RajComicsNetworkInterface.kt
@@ -4,7 +4,7 @@ import com.siderfighter.comicsinfo.data.endpoints.ApiEndpointConstants.RAJ_COMIC
 import retrofit2.Response
 import retrofit2.http.GET
 
-interface NetworkInterface {
+interface RajComicsNetworkInterface {
     @GET(RAJ_COMICS_PAGE_1)
     suspend fun getRajComicsPage1(): Response<String> // todo: Change to appropriate object
 }
