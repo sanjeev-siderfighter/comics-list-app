ecb3e1b7bb0118b84ab89b824cbb7da59dd06626
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-13 00:11:08 +0530

    chore: Wrap the api calls with withContext

--- app/src/main/java/com/siderfighter/comicsinfo/data/repository/rajcomics/RajComicsRepository.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/data/repository/rajcomics/RajComicsRepository.kt
@@ -3,22 +3,28 @@ package com.siderfighter.comicsinfo.data.repository.rajcomics
 import com.siderfighter.comicsinfo.data.endpoints.RajComicsNetworkInterface
 import com.siderfighter.comicsinfo.domain.rajcomics.IRajComicsRepository
 import com.siderfighter.comicsinfo.domain.rajcomics.IRajComicsResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RajComicsRepository @Inject constructor(
     private val networkInterface: RajComicsNetworkInterface
 ) : IRajComicsRepository {
-//    @Inject lateinit var networkInterface: RajComicsNetworkInterface
+    //    @Inject lateinit var networkInterface: RajComicsNetworkInterface
     override suspend fun getAllRajComics(): IRajComicsResponse {
-        return networkInterface.getAllRajComics()
+        return withContext(Dispatchers.IO) {
+            networkInterface.getAllRajComics()
+        }
     }
 
     override suspend fun getRajComicsByPage(page: Int): IRajComicsResponse {
-        return networkInterface.getRajComicsByPage(
-            RajComicsPageRequest(
-                page = page
+        return withContext(Dispatchers.IO) {
+            networkInterface.getRajComicsByPage(
+                RajComicsPageRequest(
+                    page = page
+                )
             )
-        )
+        }
     }
 
 
