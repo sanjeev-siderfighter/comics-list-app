90f80059595bd36b465a3e3e40d03d9a62eac43c
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-13 01:22:21 +0530

    feat: Add navigation with raj comics list fragment as the home fragment

--- app/src/main/java/com/siderfighter/comicsinfo/presentation/views/activity/MainActivity.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/presentation/views/activity/MainActivity.kt
@@ -11,23 +11,9 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private val viewModel: RajComicsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val tv = findViewById<TextView>(R.id.tv_hello)
-
-//        viewModel.getAllRajComics()
-        viewModel.getRajComicsByPage(2)
-
-        viewModel.rajComicsPage1.observe(this) {
-//            tv.text = it.data.toString()
-        }
-
-        viewModel.rajComicsByPage.observe(this) {
-            tv.text = tv.text.toString() + it.data.toString()
-        }
     }
 }
