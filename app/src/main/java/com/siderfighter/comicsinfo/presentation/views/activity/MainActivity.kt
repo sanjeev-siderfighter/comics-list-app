889d58055623c4a9f9aef024c177f3afe3db4e20
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-13 01:42:39 +0530

    feat: Add the method for getting entire raj comics list
    feat: Use flow with livedata and make the necessary changes in the usecase

--- app/src/main/java/com/siderfighter/comicsinfo/presentation/views/activity/MainActivity.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/presentation/views/activity/MainActivity.kt
@@ -2,12 +2,8 @@ package com.siderfighter.comicsinfo.presentation.views.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.viewModels
 import com.siderfighter.comicsinfo.R
-import com.siderfighter.comicsinfo.presentation.viewmodel.RajComicsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/siderfighter/comicsinfo/presentation/views/fragment/RajComicsListFragment.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/presentation/views/fragment/RajComicsListFragment.kt
@@ -1,16 +1,17 @@
 package com.siderfighter.comicsinfo.presentation.views.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.siderfighter.comicsinfo.R
 import com.siderfighter.comicsinfo.databinding.RajComicsListFragmentBinding
 import com.siderfighter.comicsinfo.presentation.viewmodel.RajComicsListViewModel
-import javax.inject.Inject
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RajComicsListFragment : Fragment() {
 
     private val viewModel: RajComicsListViewModel by viewModels()
@@ -22,6 +23,19 @@ class RajComicsListFragment : Fragment() {
     ): View {
         binding = RajComicsListFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initObservers()
+
+        viewModel.getAllRajComics()
+    }
+
+    private fun initObservers() {
+        viewModel.allRajComicsList.observe(viewLifecycleOwner) {
+            Log.d("siderfighter", "$it")
+        }
     }
 
 }
