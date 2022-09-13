11a9d640e624203bb68dcdc36c993df8f7e4ab81
siderfighter <sanjeev.siderfighter@gmail.com>
2022-09-13 23:14:52 +0530

    :sparkles: Save the list passed by list fragment to the details fragment in the details view model by observing the livedata in the shared viewmodel

--- app/src/main/java/com/siderfighter/comicsinfo/presentation/viewmodel/RajComicsDetailViewModel.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/presentation/viewmodel/RajComicsDetailViewModel.kt
@@ -1,7 +1,13 @@
 package com.siderfighter.comicsinfo.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.siderfighter.comicsinfo.domain.rajcomics.RajComicsListModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class RajComicsDetailViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class RajComicsDetailViewModel
+@Inject
+constructor() : ViewModel() {
+    var rajComicsList: RajComicsListModel = RajComicsListModel(emptyList())
 }
--- app/src/main/java/com/siderfighter/comicsinfo/presentation/views/fragment/RajComicsDetailFragment.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/presentation/views/fragment/RajComicsDetailFragment.kt
@@ -2,19 +2,25 @@ package com.siderfighter.comicsinfo.presentation.views.fragment
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.siderfighter.comicsinfo.R
 import com.siderfighter.comicsinfo.databinding.RajComicsDetailFragmentBinding
 import com.siderfighter.comicsinfo.domain.rajcomics.RajComicsListItemModel
+import com.siderfighter.comicsinfo.presentation.viewmodel.MainViewModel
 import com.siderfighter.comicsinfo.presentation.viewmodel.RajComicsDetailViewModel
 
 class RajComicsDetailFragment : Fragment() {
 
-    private lateinit var viewModel: RajComicsDetailViewModel
+    private val viewModel: RajComicsDetailViewModel by viewModels()
+    private val sharedViewModel: MainViewModel by activityViewModels()
+
     private lateinit var binding: RajComicsDetailFragmentBinding
     private val args by navArgs<RajComicsDetailFragmentArgs>()
 
@@ -29,6 +35,14 @@ class RajComicsDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUi()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        sharedViewModel.rajComicsListLiveData.observe(viewLifecycleOwner) {
+            Log.d("siderfighter", "observed shared view model")
+            viewModel.rajComicsList = it
+        }
     }
 
     private fun setupUi() {
