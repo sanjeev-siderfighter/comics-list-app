500fd775a9956bb0d96bb03b733460419c67a1b5
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-01-15 16:20:18 +0530

    :style: :poop: Show complete comic number in the comics detail fragment. But it needs to be fixed later.

--- app/src/main/java/com/siderfighter/comicsinfo/presentation/viewmodel/RajComicsListViewModel.kt
+++ app/src/main/java/com/siderfighter/comicsinfo/presentation/viewmodel/RajComicsListViewModel.kt
@@ -1,8 +1,6 @@
 package com.siderfighter.comicsinfo.presentation.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.siderfighter.comicsinfo.domain.rajcomics.RajComicsListModel
